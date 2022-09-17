5f0cdaf30b11478443f1bad91da2ffd02e172f87
Fernando Diaz Taballione <fdtaballione@gmail.com>
2022-09-17 17:56:31 -0300

    botones agregar y disminuir cantidad en tarjeta de productos OK

--- src/componets/Productos.js
+++ src/componets/Productos.js
@@ -1,35 +1,37 @@
- 
- import React from 'react'
- import TarjetaProducto from './TarjetaProducto'
- import CarrousellProd from "./CarrousellProd.js"
- 
- const classContainer = "bg-amber-200 w-full h-screen flex flex-col items-center justify-center"
+import { React } from "react";
+import TarjetaProducto from "./TarjetaProducto";
+import CarrousellProd from "./CarrousellProd.js";
 
- const producto = [ {
-  nombre: "Alimento Balanceado Vital Balance",
-  descripcion: "Bolsa de 20 kg para razas medianas con alto contenido proteico",
-  precio: 7800,
-  imagen: 'Prod01.jpg'
-},
-{
-  nombre: "Alimento Balanceado Royal Canin",
-  descripcion: "Bolsa de 10 kg para razas Pequeñas con alto contenido proteico",
-  precio: 4500,
-  imagen: 'Prod02.jpg'
-}
-]
+const classContainer =
+  "bg-amber-200 w-full h-screen flex flex-col items-center justify-center";
 
+const producto = [
+  {
+    nombre: "Alimento Balanceado Vital Balance",
+    descripcion:
+      "Bolsa de 20 kg para razas medianas con alto contenido proteico",
+    precio: 7800,
+    imagen: "Prod01.jpg",
+  },
+  {
+    nombre: "Alimento Balanceado Royal Canin",
+    descripcion:
+      "Bolsa de 10 kg para razas Pequeñas con alto contenido proteico",
+    precio: 4500,
+    imagen: "Prod02.jpg",
+  },
+];
 
- const Productos = () => {
+const Productos = () => {
   return (
     <>
-    <div className={classContainer}>
-          <TarjetaProducto producto={producto[0]}/>
-          <TarjetaProducto producto={producto[1]}/>
-    </div>
-    <CarrousellProd/>
+      <div className={classContainer}>
+        <TarjetaProducto producto={producto[0]} />
+        <TarjetaProducto producto={producto[1]} />
+      </div>
+      <CarrousellProd />
     </>
-  )
- }
- 
- export default Productos
+  );
+};
+
+export default Productos;
--- src/componets/TarjetaProducto.js
+++ src/componets/TarjetaProducto.js
@@ -1,14 +1,18 @@
-import React from "react";
-
+import { React, useState } from "react";
 
 const classContenedorTarjera =
   "w-full h-56 flex flex-row shadow-xl bg-white my-1 ";
 
-  const classBotonMasMenos =
-  "bg-orange-800 rounded-full font-black w-8 h-8 hover:bg-orange-700 text-lg"
+const classBotonMasMenos =
+  "bg-orange-800 rounded-full font-black w-8 h-8 hover:bg-orange-700 text-lg";
 
+const classBotonMasMenosDisablesd =
+  "bg-orange-200 rounded-full font-black w-8 h-8 text-lg";
 const TarjetaProducto = (props) => {
   const producto = props.producto;
+  const [cantidad, setCantidad] = useState(1);
+  const agregar = () => setCantidad(cantidad + 1);
+  const disminuir = () => setCantidad(cantidad - 1);
 
   return (
     <>
@@ -22,29 +26,40 @@ const TarjetaProducto = (props) => {
         </div>
         <div className="flex flex-col sm:flex-row items-center h-full w-full">
           <div className="flex flex-col w-full px-2 sm:w-4/6">
-                <div className="font-serif text-xl font-bold">
-                     {producto.nombre}
-                </div>
-                <p className="font-serif">{producto.descripcion}</p>
-                <div className="text-blue-600 font-bold">$ {producto.precio}</div>
+            <div className="font-serif text-xl font-bold">
+              {producto.nombre}
+            </div>
+            <p className="font-serif">{producto.descripcion}</p>
+            <div className="text-blue-600 font-bold">$ {producto.precio}</div>
           </div>
           <div className="w-full flex flex-col justify-center px-4  bg-white h-full w-full sm:w-2/6 ">
-                <div className="flex flex-row items-center justify-center sm:flex-col  ">
-                    <div className="flex flex-row mr-6 sm:mr-0 sm:mb-2 w-full justify-center content-center ">
-                       <button className={classBotonMasMenos}>+</button>
-                       <div className="px-3">1</div>
-                       <button className={classBotonMasMenos}>-</button>
-                    </div>
-                    <div className=" flex w-full justify-center ">
-                        <button className="shadow-xl border-2 border-orange-400 p-1 rounded bg-orange-500 hover:bg-orange-200">
-                             Agregar a carrito
-                        </button>
-                    </div>
-                </div>
+            <div className="flex flex-row items-center justify-center sm:flex-col  ">
+              <div className="flex flex-row mr-6 sm:mr-0 sm:mb-2 w-full justify-center content-center ">
+                <button className={classBotonMasMenos} onClick={agregar}>
+                  +
+                </button>
+                <div className="px-3">{cantidad}</div>
+                <button
+                  className={
+                    cantidad <= 1
+                      ? classBotonMasMenosDisablesd
+                      : classBotonMasMenos
+                  }
+                  onClick={disminuir}
+                  disabled={cantidad <= 1}
+                >
+                  -
+                </button>
+              </div>
+              <div className=" flex w-full justify-center ">
+                <button className="shadow-xl border-2 border-orange-400 p-1 rounded bg-orange-500 hover:bg-orange-200">
+                  Agregar a carrito
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
     </>
   );
 };
